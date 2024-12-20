a777a9b91cf726b8f94e1646440ee874f1fc4263
Nicolas GUYOMARCH <nicolas.guyomarch@etu.unistra.fr>
2024-12-20 13:52:28 +0100

    Creation des projets

---
+++ Puissance4UniT/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿namespace Puissance4UniT
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var test = true;
+
+            Assert.True(test);
+        }
+    }
+}
