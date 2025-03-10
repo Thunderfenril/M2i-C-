9c3ec7fde66653c8d44f5476c662f4d0b543f631
Nicolas GUYOMARCH <nicolasguyomarch67@gmail.com>
2025-03-10 14:07:07 +0100

    Premier commit pour une version visuel

---
+++ MorpionVisuel/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MorpionVisuel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ MorpionVisuel/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace MorpionVisuel
+{
+    public partial class MainPage : ContentPage
+    {
+        int counter = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            var row = Grid.GetRow(button);
+            var column = Grid.GetColumn(button);
+            var texte = button.Text;
+            System.Diagnostics.Debug.WriteLine("Ligne: " + row +"\nColonne: " + column + "\nTexte :" + texte);
+
+
+            if (texte == "O" || texte == "X")
+            {
+                return;
+            }
+            button.Text = counter % 2 == 0 ? "O" : "X";
+
+            counter++;
+        }
+    }
+
+}
